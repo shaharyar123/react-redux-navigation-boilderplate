66bc06d1b62bb0e13105c81f6fe9eedcba2af2d8
Muhammad Shaharyar <shaharyar@loopsdigital.com>
2020-11-14 17:19:39 +0500

    project updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,11 +10,11 @@ import Route from './Route';
 export default function Routes() {
     return (
         <Switch>
-            <Route path="/" exact component={SignIn} />
-            <Route path="/register" component={SignUp} />
-            <Route path="/dashboard" component={Dashboard} isPrivate />
+            <Route path="/" exact component={props => <SignIn {...props} />} />
+            <Route path="/register" component={props => <SignUp {...props} />} />
+            <Route path="/dashboard" component={props => <Dashboard {...props} />} isPrivate />
             {/* redirect user to SignIn page if route does not exist and user is not authenticated */}
-            <Route component={SignIn} />
+            <Route component={props => <SignIn {...props} />} />
         </Switch>
     );
 }
